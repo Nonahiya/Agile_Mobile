b69d4cddd1c42220b1476e25f06617e14ee93e0c
Nonahiya <enricowakiman9@gmail.com>
2023-06-02 18:52:02 +0700

    helper?

--- lib/shared/helper.dart
+++ lib/shared/helper.dart
@@ -14,4 +14,37 @@ class Helper {
 
     return unixTimestamp.toString();
   }
+
+  static String toUnixDateTime(String dateString) {
+    List<String> dateTimeParts = dateString.split(' ');
+
+    List<String> dateParts = dateTimeParts[0].split('/');
+    int day = int.parse(dateParts[0]);
+    int month = int.parse(dateParts[1]);
+    int year = int.parse(dateParts[2]);
+
+    List<String> timeParts = dateTimeParts[1].split(':');
+    int hour = int.parse(timeParts[0]);
+    int minutes = int.parse(timeParts[1]);
+
+    DateTime date = DateTime(year, month, day, hour, minutes);
+
+    print(date);
+
+    int unixTimestamp = date.millisecondsSinceEpoch ~/ 1000;
+    
+    print(unixTimestamp);
+
+    return unixTimestamp.toString();
+  }
+
+  static String fromUnix(String dateString) {
+  DateFormat inputDateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
+  DateTime dateTime = inputDateFormat.parse(dateString);
+  Duration offset = const Duration(hours: 7); // GMT+7 offset
+  DateTime adjustedDateTime = dateTime.add(offset);
+  DateFormat outputDateFormat = DateFormat('dd/MM/yyyy HH:mm');
+  String dateTimeString = outputDateFormat.format(adjustedDateTime);
+  return dateTimeString;
+  }
 }
